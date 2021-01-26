39e155f22446d75b69340f9b66db021b3147f5f0
Kyle Benson <kyle@psadmin.io>
2021-01-25 18:49:14 -0600

    Converted json payload to Message Card format
    Message Card format: https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/connectors-using

--- src/MicrosoftTeamsNotification.groovy
+++ src/MicrosoftTeamsNotification.groovy
@@ -1,7 +1,6 @@
 import com.dtolabs.rundeck.plugins.notification.NotificationPlugin
 import groovy.json.JsonOutput
 
-
 rundeckPlugin(NotificationPlugin){
     title="Microsoft Teams notification Plugin"
     description="Allows to set up notification for Microsoft Teams chats for a channel, via Webhook URL. To use it you will have to obtain webhook for your channel first and setit up."
@@ -13,65 +12,144 @@ rundeckPlugin(NotificationPlugin){
     onstart {
         type = "START"
         color = "696969"
+        //Single argument, the configuration properties are available automatically
+// sections:         "activityImage": "https://sample.com/image.png",
         json_payload = JsonOutput.toJson([
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            summary: "Rundeck Job Notification",
-            text: "$job id: #${execution.job.id}, job project: ${execution.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
+            "@type": "MessageCard",
+            "@context": "http://schema.org/extensions",
+            "themeColor": "${color}",
+            "summary": "${execution.project}:${execution.job.group}:${execution.job.name}",
+            "sections": [[
+                "activityTitle": "${execution.job.name}",
+                "activitySubtitle": "${execution.project}:${execution.job.group}",
+                "activityImage": "https://www.samplecom/image.png",
+                "facts": [[
+                    "name": "Job ID",
+                    "value": "#${execution.job.id}"
+                ], [
+                    "name": "Execution ID",
+                    "value": "#${execution.id}"
+                ], [
+                    "name": "Execution Status",
+                    "value": "#${execution.status}"
+                ], [
+                    "name": "Options",
+                    "value": "${execution.context.option}"
+                ], [
+                    "name": "Started",
+                    "value": "${execution.dateStarted}"
+                ], [
+                    "name": "Ended",
+                    "value": "${execution.dateEnded}"
+                ]],
+                "markdown": true
+            ]],
+            "potentialAction": [[
+                "@type": "OpenUri",
+                "name": "View in Rundeck",
+                "targets": [[
+                    "os": "default",
+                    "uri": "${execution.href}"
+                ]]
+            ]]
         ])
         process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
 
         return true
     }
 
+// sections:         "activityImage": "https://sample.com/image.png",
     onfailure {
         type = "FAILURE"
         color = "E81123"
-        //Single argument, the configuration properties are available automatically
+        //with no args, there is a "configuration" and an "execution" variable in the context
         json_payload = JsonOutput.toJson([
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            summary: "Rundeck Job Notification",
-            text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
+            "@type": "MessageCard",
+            "@context": "http://schema.org/extensions",
+            "themeColor": "${color}",
+            "summary": "${execution.project}:${execution.job.group}:${execution.job.name}",
+            "sections": [[
+                "activityTitle": "${execution.job.name}",
+                "activitySubtitle": "${execution.project}:${execution.job.group}",
+                "activityImage": "https://www.samplecom/image.png",
+                "facts": [[
+                    "name": "Job ID",
+                    "value": "#${execution.job.id}"
+                ], [
+                    "name": "Execution ID",
+                    "value": "#${execution.id}"
+                ], [
+                    "name": "Execution Status",
+                    "value": "#${execution.status}"
+                ], [
+                    "name": "Options",
+                    "value": "${execution.context.option}"
+                ], [
+                    "name": "Started",
+                    "value": "${execution.dateStarted}"
+                ], [
+                    "name": "Ended",
+                    "value": "${execution.dateEnded}"
+                ]],
+                "markdown": true
+            ]],
+            "potentialAction": [[
+                "@type": "OpenUri",
+                "name": "View in Rundeck",
+                "targets": [[
+                    "os": "default",
+                    "uri": "${execution.href}"
+                ]]
+            ]]
         ])
         process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
 
         return true
     }
 
+    // sections:         "activityImage": "https://sample.com/image.png",
     onsuccess {
         type = "SUCCESS"
         color = "228B22"
         //with no args, there is a "configuration" and an "execution" variable in the context
         json_payload = JsonOutput.toJson([
-            title: "Rundeck Job Notification",
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
+            "@type": "MessageCard",
+            "@context": "http://schema.org/extensions",
+            "themeColor": "${color}",
+            "summary": "${execution.project}:${execution.job.group}:${execution.job.name}",
+            "sections": [[
+                "activityTitle": "${execution.job.name}",
+                "activitySubtitle": "${execution.project}:${execution.job.group}",
+                "activityImage": "https://www.samplecom/image.png",
+                "facts": [[
+                    "name": "Job ID",
+                    "value": "#${execution.job.id}"
+                ], [
+                    "name": "Execution ID",
+                    "value": "#${execution.id}"
+                ], [
+                    "name": "Execution Status",
+                    "value": "#${execution.status}"
+                ], [
+                    "name": "Options",
+                    "value": "${execution.context.option}"
+                ], [
+                    "name": "Started",
+                    "value": "${execution.dateStarted}"
+                ], [
+                    "name": "Ended",
+                    "value": "${execution.dateEnded}"
+                ]],
+                "markdown": true
+            ]],
+            "potentialAction": [[
+                "@type": "OpenUri",
+                "name": "View in Rundeck",
+                "targets": [[
+                    "os": "default",
+                    "uri": "${execution.href}"
+                ]]
+            ]]
         ])
         process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
 
